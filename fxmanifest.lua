2a994924c3f0e4954ca125c120be164e44f58887
a_dev.bat <147559457+aDevBat@users.noreply.github.com>
2023-10-16 18:16:49 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+    'client/main.lua',
+    'client/shop.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+}
+
+shared_script 'config.lua'
